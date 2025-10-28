c37fb37ff7db73172aa030b809ec1a7eb8653778
Donavin Tzunun <tzunundonavin@gmail.com>
2025-10-28 02:48:49 -0600

    Add sanction validation to authService

--- ecommerce-api/src/main/java/com/navi/ecommerceapi/service/AuthService.java
+++ ecommerce-api/src/main/java/com/navi/ecommerceapi/service/AuthService.java
@@ -3,23 +3,29 @@ package com.navi.ecommerceapi.service;
 import com.navi.ecommerceapi.dto.UserCredentialDto;
 import com.navi.ecommerceapi.exception.DomainException;
 import com.navi.ecommerceapi.exception.InvalidCredentialsException;
+import com.navi.ecommerceapi.model.Sanction;
 import com.navi.ecommerceapi.model.User;
+import com.navi.ecommerceapi.repository.SanctionRepository;
 import io.jsonwebtoken.*;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
+import java.time.LocalDateTime;
 import java.util.Date;
+import java.util.List;
 
 @Service
 public class AuthService {
     private final UserService userService;
+    private final SanctionRepository sanctionRepository;
     private final BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder(11);
     private final SecretKey key = Keys.hmacShaKeyFor("mysecretkeymysecretkeymysecretkey12".getBytes());
 
-    public AuthService(UserService userService) {
+    public AuthService(UserService userService,  SanctionRepository sanctionRepository) {
         this.userService = userService;
+        this.sanctionRepository = sanctionRepository;
     }
 
     public String login(UserCredentialDto credentials) {
@@ -28,6 +34,7 @@ public class AuthService {
         if (user == null) throw new InvalidCredentialsException("wrong_credentials");
         if (!user.getActive()) throw new DomainException("inactive_user");
         if (!passwordEncoder.matches(credentials.getPassword(), user.getPassword())) throw new InvalidCredentialsException("wrong_credentials");
+        if (isUserSanctioned(user)) throw new DomainException("user_sanctioned");
 
         long expirationMs = 24 * 60 * 60 * 1000;
         return Jwts.builder()
@@ -38,6 +45,13 @@ public class AuthService {
                 .claim("role", user.getRole().getRoleId())
                 .signWith(key)
                 .compact();
+    }
+
+    private boolean isUserSanctioned(User user) {
+        List<Sanction> sanctions = sanctionRepository.findByUserUserId(user.getUserId());
+        LocalDateTime now = LocalDateTime.now();
+
+        return sanctions.stream().anyMatch(s -> s.getEndDate() != null && !s.getEndDate().isBefore(now));
     }
 
     public Jws<Claims> verifyToken(String token) {
